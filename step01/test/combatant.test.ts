55dc8668c2d8a36da081fbf33988fcf5fe138de9
futayama <futa.ymkw@gmail.com>
2026-08-20 19:38:45 +0900

    improve: instance名修正

--- step01/test/combatant.test.ts
+++ step01/test/combatant.test.ts
@@ -1,8 +1,8 @@
 import { Combatant } from "../src/combatant";
 
 test("戦闘キャラを作成できる", () => {
-  const combatant = new Combatant("勇者", 100, 20);
-  expect(combatant).toBeInstanceOf(Combatant);
+  const hero = new Combatant("勇者", 100, 20);
+  expect(hero).toBeInstanceOf(Combatant);
 });
 
 test("name空の戦闘キャラを作成できない", () => {
@@ -22,33 +22,33 @@ test("攻撃力が負の戦闘キャラを作成できない", () => {
 });
 
 test("攻撃者のstrがターゲットのhpより小さい場合、str分の攻撃をする", () => {
-  const attacker = new Combatant("勇者", 100, 20);
-  const target = new Combatant("スライム", 30, 5);
+  const hero = new Combatant("勇者", 100, 20);
+  const slime = new Combatant("スライム", 30, 5);
 
-  attacker.attack(target);
+  hero.attack(slime);
 
-  expect(target.hp).toBe(10);
+  expect(slime.hp).toBe(10);
 });
 
 test("攻撃者のstrがターゲットのhpより大きい場合、hpは0で止まる", () => {
-  const attacker = new Combatant("勇者", 100, 20);
-  const target = new Combatant("スライム", 30, 5);
+  const hero = new Combatant("勇者", 100, 20);
+  const slime = new Combatant("スライム", 30, 5);
 
-  attacker.attack(target);
-  expect(target.hp).toBe(10);
+  hero.attack(slime);
+  expect(slime.hp).toBe(10);
 
-  attacker.attack(target);
-  expect(target.hp).toBe(0);
+  hero.attack(slime);
+  expect(slime.hp).toBe(0);
 
-  expect(() => attacker.attack(target)).toThrow(new Error("target is dead"));
+  expect(() => hero.attack(slime)).toThrow(new Error("target is dead"));
 });
 
 test("死んだ戦闘キャラは攻撃できない", () => {
-  const attacker = new Combatant("勇者", 100, 20);
-  const target = new Combatant("ボス", 300, 80);
+  const hero = new Combatant("勇者", 100, 20);
+  const boss = new Combatant("ボス", 300, 80);
 
-  target.attack(attacker);
-  target.attack(attacker);
+  boss.attack(hero);
+  boss.attack(hero);
 
-  expect(() => attacker.attack(target)).toThrow("attacker is dead");
+  expect(() => hero.attack(boss)).toThrow("attacker is dead");
 });
--- step02/test/combatant.test.ts
+++ step02/test/combatant.test.ts
@@ -1,8 +1,8 @@
 import { Combatant } from "../src/combatant";
 
 test("戦闘キャラを作成できる", () => {
-  const combatant = new Combatant("勇者", 100, 20, 5);
-  expect(combatant).toBeInstanceOf(Combatant);
+  const hero = new Combatant("勇者", 100, 20, 5);
+  expect(hero).toBeInstanceOf(Combatant);
 });
 
 test("name空の戦闘キャラを作成できない", () => {
@@ -28,49 +28,49 @@ test("防御力が負の戦闘キャラを作成できない", () => {
 });
 
 test("攻撃者のstrがターゲットのhpより小さい場合、str分の攻撃をする", () => {
-  const attacker = new Combatant("勇者", 100, 20, 5);
-  const target = new Combatant("スライム", 30, 5, 0);
+  const hero = new Combatant("勇者", 100, 20, 5);
+  const slime = new Combatant("スライム", 30, 5, 0);
 
-  attacker.attack(target);
+  hero.attack(slime);
 
-  expect(target.hp).toBe(10);
+  expect(slime.hp).toBe(10);
 });
 
 test("攻撃者のstrがターゲットのhpより大きい場合、hpは0で止まる", () => {
-  const attacker = new Combatant("勇者", 100, 20, 5);
-  const target = new Combatant("スライム", 30, 5, 0);
+  const hero = new Combatant("勇者", 100, 20, 5);
+  const slime = new Combatant("スライム", 30, 5, 0);
 
-  attacker.attack(target);
-  expect(target.hp).toBe(10);
+  hero.attack(slime);
+  expect(slime.hp).toBe(10);
 
-  attacker.attack(target);
-  expect(target.hp).toBe(0);
+  hero.attack(slime);
+  expect(slime.hp).toBe(0);
 
-  expect(() => attacker.attack(target)).toThrow(new Error("target is dead"));
+  expect(() => hero.attack(slime)).toThrow(new Error("target is dead"));
 });
 
 test("死んだ戦闘キャラは攻撃できない", () => {
-  const attacker = new Combatant("勇者", 100, 20, 5);
-  const target = new Combatant("ボス", 300, 80, 0);
+  const hero = new Combatant("勇者", 100, 20, 5);
+  const boss = new Combatant("ボス", 300, 80, 0);
 
-  target.attack(attacker);
-  target.attack(attacker);
+  boss.attack(hero);
+  boss.attack(hero);
 
-  expect(() => attacker.attack(target)).toThrow("attacker is dead");
+  expect(() => hero.attack(boss)).toThrow("attacker is dead");
 });
 
 test("防御力の分だけダメージを減らす", () => {
-  const attacker = new Combatant("勇者", 100, 20, 5);
-  const target = new Combatant("オーク", 30, 10, 0);
+  const hero = new Combatant("勇者", 100, 20, 5);
+  const orc = new Combatant("オーク", 30, 10, 0);
 
-  target.attack(attacker);
-  expect(attacker.hp).toBe(95);
+  orc.attack(hero);
+  expect(hero.hp).toBe(95);
 });
 
 test("防御力が相手の攻撃力より高いとき、ダメージを受けない", () => {
-  const attacker = new Combatant("勇者", 100, 20, 10);
-  const target = new Combatant("スライム", 30, 5, 0);
+  const hero = new Combatant("勇者", 100, 20, 10);
+  const slime = new Combatant("スライム", 30, 5, 0);
 
-  target.attack(attacker);
-  expect(attacker.hp).toBe(100);
+  slime.attack(hero);
+  expect(hero.hp).toBe(100);
 });
